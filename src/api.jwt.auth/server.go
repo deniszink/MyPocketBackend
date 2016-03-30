a080fdcfc7b2d11250869f1cbeed8bc9a179c590
Denis Zinkovskiy <dennis.zink.ua@gmail.com>
2016-03-30 10:06:11 +0300

    heroku 3 try

--- src/api.jwt.auth/server.go
+++ src/api.jwt.auth/server.go
@@ -4,22 +4,13 @@ import (
 	"api.jwt.auth/settings"
 	"api.jwt.auth/routers"
 	"github.com/codegangsta/negroni"
-	"net/http"
-
-	"os"
-	"log"
 )
 
 func main() {
-	port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
 	settings.Init()
 	router := routers.InitRoutes()
 	n := negroni.Classic()
 	n.UseHandler(router)
-	http.ListenAndServe(":"+port,n)
 }
 
