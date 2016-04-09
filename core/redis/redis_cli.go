90b5dc9d451ffc85cb0b449cdb82bea8bf671bb4
Denis Zinkovskiy <dennis.zink.ua@gmail.com>
2016-04-09 16:50:15 +0300

    try to run dub 9

--- core/redis/redis_cli.go
+++ core/redis/redis_cli.go
@@ -17,7 +17,7 @@ func Connect() (conn *RedisCli) {
 		instanceRedisCli = new(RedisCli)
 		var err error
 
-		instanceRedisCli.conn, err = redis.Dial("tcp", os.Getenv("REDISTOGO_URL"))
+		instanceRedisCli.conn, err = redis.Dial("tcp", "@lab.redistogo.com:9951")
 
 		if err != nil {
 			panic(err)
@@ -27,6 +27,7 @@ func Connect() (conn *RedisCli) {
 			instanceRedisCli.conn.Close()
 			panic(err)
 		}
+
 	}
 
 	return instanceRedisCli
