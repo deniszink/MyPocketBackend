c84d01c5746eeb21b0e5a6a753ddaf713db15095
Denis Zinkovskiy <dennis.zink.ua@gmail.com>
2016-04-25 22:58:43 +0300

    cleanup

--- controllers/registration_controller.go
+++ controllers/registration_controller.go
@@ -15,8 +15,8 @@ func Registration(w http.ResponseWriter, r *http.Request) {
 	decode.Decode(&newUser)
 
 	fmt.Println(newUser)
-	data,_ :=ioutil.ReadAll(r.Body)
-	fmt.Printf("body = %s",string(data))
+	data, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("body = %s", string(data))
 
 	responseStatus, body := services.Registration(newUser)
 	w.Header().Set("Content-Type", "application/json")
