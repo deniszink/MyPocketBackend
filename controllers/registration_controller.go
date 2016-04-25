9ba99b14ba5a99995b3e52140494575776f5e646
Denis Zinkovskiy <dennis.zink.ua@gmail.com>
2016-04-25 20:11:50 +0300

    add logs

--- controllers/registration_controller.go
+++ controllers/registration_controller.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"backend/services"
 	"fmt"
+	"io/ioutil"
 )
 
 func Registration(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +15,8 @@ func Registration(w http.ResponseWriter, r *http.Request) {
 	decode.Decode(&newUser)
 
 	fmt.Println(newUser)
+	data,_ :=ioutil.ReadAll(r.Body)
+	fmt.Printf("body = %s",string(data))
 
 	responseStatus, body := services.Registration(newUser)
 	w.Header().Set("Content-Type", "application/json")
