682add000b417864837f555beaaeead0bf69068e
Denis Zinkovskiy <dennis.zink.ua@gmail.com>
2016-04-27 17:01:48 +0300

    response fix

--- models/users.go
+++ models/users.go
@@ -3,7 +3,7 @@ package models
 import "gopkg.in/mgo.v2/bson"
 
 type User struct {
-	Id       bson.ObjectId `bson:"id,omitempty"`
+	Id       bson.ObjectId `bson:"_id,omitempty"`
 	UUID     string `json:"uuid" form:"-"`
 	Username string `json:"username" form:"username"`
 	Email    string `json:"email" form:"email"`
