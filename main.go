2c19a58f1e76add5dd6c19cf62bf2bbf9b0006e7
Denis Zinkovskiy <dennis.zink.ua@gmail.com>
2016-04-30 18:28:08 +0300

    code cleanup

--- main.go
+++ main.go
@@ -14,10 +14,6 @@ func main() {
 	router := routers.InitRoutes()
 	n := negroni.Classic()
 	n.UseHandler(router)
-
 	http.ListenAndServe(":"+os.Getenv("PORT"), n)
-	/*connect := redis.Connect()
-	connect.SetValue("token","eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0NjIxNzkxMzcsImlhdCI6MTQ2MTkxOTkzNywic3ViIjoiIn0.JMNC18eVg9ZRbIywzZIc4jxSlrxJhnD-R9gPI_B48EGdFaOVuJm4b0Uk6LOHHjsvIh1NaJI39l1v94gih0cQcZP7j8OPdtrIt_ZDxSY499XY5dbgZMS1ftg-bk0VsP9YrAYZjhZ-8yesQlT9qpFliaGPHovdltgI9oFPURmIOOtumZTEPary-1X_Kt78vrfoz1TNEAwOuFXtbSaozFgVFiC_o6qsMqcS6fhfw9aOITa3yerQeVgKwKOk1F80i1zE7LacsYaPxqWMz9CR9cSHxuRVlsl1_nWEP_bZW8krP6wxg70x-L1Kze1yCO8SQ9ZTvak7DaublTwxOY83-hlOAQ")
-	fmt.Println(connect.GetValue("token"))*/
 }
 
