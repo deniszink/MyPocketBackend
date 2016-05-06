72d22cae5d687803fb91b10cb2e23f5e3328389c
Denis Zinkovskiy <dennis.zink.ua@gmail.com>
2016-05-06 14:52:20 +0300

    change json Id to id

--- models/Wallet.go
+++ models/Wallet.go
@@ -3,7 +3,7 @@ package models
 import "gopkg.in/mgo.v2/bson"
 
 type Wallet struct {
-	Id         bson.ObjectId `bson:"_id,omitempty"`
+	Id         bson.ObjectId `bson:"_id,omitempty" json:"id"`
 	WalletName string `json:"walletName" form:"walletName"`
 	Currency   string `json:"currency" form:"currency"`
 	Balance    float64 `json:"balance" form:"balance"`
