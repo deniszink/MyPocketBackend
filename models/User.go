35bb3e0d3c27b606dfb44670a7f0ee8f549660b2
Denis Zinkovskiy <dennis.zink.ua@gmail.com>
2016-05-19 15:54:05 +0300

    fix ids 3

--- models/User.go
+++ models/User.go
@@ -3,7 +3,7 @@ package models
 import "gopkg.in/mgo.v2/bson"
 
 type User struct {
-	Id       bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	Id       bson.ObjectId `bson:"_id,omitempty" json:"_id"`
 	Username string `json:"username" form:"username"`
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
