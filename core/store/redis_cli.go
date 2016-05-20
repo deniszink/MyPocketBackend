d80a23c5304559a7e3bff135d2e83791cd2fcea0
Denis Zinkovskiy <dennis.zink.ua@gmail.com>
2016-05-20 08:54:56 +0300

    code cleanup

--- core/store/redis_cli.go
+++ core/store/redis_cli.go
@@ -14,8 +14,8 @@ func Connect() (conn *RedisCli) {
 	if instanceRedisCli == nil {
 		instanceRedisCli = new(RedisCli)
 		var err error
-		//this is works!!!
-		instanceRedisCli.conn, err = redis.Dial("tcp", ":6379")
+
+		instanceRedisCli.conn, err = redis.Dial("tcp", "lab.redistogo.com:9951")
 
 		if err != nil {
 			panic(err)
